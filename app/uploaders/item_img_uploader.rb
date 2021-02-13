5441bc2b12267f9ffeaf9ccad6afc917d5e97272
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2021-02-13 09:59:39 +0000

    error fix if文間違えたので修正

--- app/uploaders/item_img_uploader.rb
+++ app/uploaders/item_img_uploader.rb
@@ -16,7 +16,7 @@ class ItemImgUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    Rails.env.production?
+    if Rails.env.production?
       "assets/imgs/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     else
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
