8d7d6361f2a6995c8f7736075adc2e08ae77bd60
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2021-03-29 10:06:17 +0000

    [update] ゲストユーザーがログインするたびに通知を未読に戻す

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,10 @@ class Users::SessionsController < Devise::SessionsController
   def new_guest
     user = User.guest
     sign_in user
+    # ゲストユーザーへの通知を未読に戻す
+    notifications = current_user.passive_notifications.where(checked: true)
+    notifications.update(checked: false) if notifications.any?
+
     flash[:success] = "ゲストユーザーとしてログインしました"
     redirect_to user_path(user)
   end
